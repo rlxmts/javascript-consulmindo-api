f93210c13ff85a585d5d2c9c7824e552d8bc37be
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-04-11 17:21:57 -0300

    Inserindo busca de videos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const listaVideos = document.querySelector('.videos__container');
+const barraPesquisa = document.querySelector('.pesquisar__input');
+const listaCategorias = document.querySelectorAll('.superior__item');
 
 async function buscarVideos() {
     try{
@@ -8,19 +10,40 @@ async function buscarVideos() {
         videos.forEach( (video) => {
             listaVideos.innerHTML += `
             <li class="videos__item">
-                <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>  
+            <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>  
                 <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="logo do canal">
-                    <h3>${video.titulo}</h3>
-                    <p>${video.descricao}</p>
+                <img class="img-canal" src="${video.imagem}" alt="logo do canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p>${video.descricao}</p>
+                <p class="video-categoria" hidden>${video.categoria}</p>
                 </div>
-            </li>
-            `
-        })
-    }catch(erro){
-        listaVideos.innerHTML = `<p>Não foi possivel carregar os videos: ${erro}</p>`
-    }
-};
-;
-
+                </li>
+                `
+            })
+        }catch(erro){
+            listaVideos.innerHTML = `<p>Não foi possivel carregar os videos: ${erro}</p>`
+        }
+    };
+    
 buscarVideos();
+
+function filtrarVideos(){
+    const videos = document.querySelectorAll('.videos__item');
+    if(barraPesquisa != ""){
+        videos.forEach( video => {
+            let titulo = video.querySelector('.titulo-video').textContent.toLocaleLowerCase();
+            let valorFiltro = barraPesquisa.value.toLocaleLowerCase();
+            
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = 'none';
+            }else{
+                video.style.display = 'block';
+            }
+        })
+    }else{
+        videos.style.display = 'block';
+    }
+}
+barraPesquisa.addEventListener( 'input', filtrarVideos);
+
+
